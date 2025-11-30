d2ad4f5cc4a5536ac53658fa5a95e4290f90b3f1
fauriatjules-max <fauriatjules@gmail.com>
2025-11-30 15:26:51 +0100

    Create vite.config.ts

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      exclude: ['**/*.test.ts', '**/*.spec.ts', '**/*.stories.tsx']
+    })
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'ProVideoEditorUI',
+      fileName: (format) => `provideoeditor-ui.${format}.js`,
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
+    sourcemap: true,
+    minify: 'esbuild'
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase'
+    },
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/variables.scss";'
+      }
+    }
+  }
+});
